d72d1d944eac90b1a47129b6d100fd3c3c571450
Mats <mats.becker@gmail.com>
2018-09-03 21:39:17 +0200

    Added icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Collapse, Button, CardBody, Card } from 'reactstrap';
 import { Link } from 'react-router-dom'
 
+import { IoIosBook } from "react-icons/io";
+
+
 class NavBar extends Component {
   constructor(props) {
     super(props);
@@ -38,8 +41,8 @@ class NavBar extends Component {
   <div class="navbar navbar-dark bg-dark shadow-sm">
     <div class="container d-flex justify-content-between">
       <a href="#" class="navbar-brand d-flex align-items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg>
-        <strong>Album</strong>
+        <IoIosBook style={{marginRight: 10 + "px"}}/>
+        <strong>Books</strong>
       </a>
       
       <Button class="navbar-toggler collapsed" onClick={this.toggle} >
